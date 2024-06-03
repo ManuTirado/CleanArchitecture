3b56d00ebc19ccea69bef368ee23359fd5c60418
manueltirado <manueltirado@JH9KW4RWNH-A098.local>
2024-06-03 21:34:10 +0200

    Added Domain Error

---
+++ CleanArchitecture/CleanArchitecture/Domain/CryptocurrencyDomainError.swift
@@ -0,0 +1,12 @@
+//
+//  CryptocurrencyDomainError.swift
+//  CleanArchitecture
+//
+//  Created by manueltirado on 3/6/24.
+//
+
+import Foundation
+
+enum CryptocurrencyDomainError: Error {
+    case generic
+}
