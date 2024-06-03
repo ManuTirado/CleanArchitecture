aff2b9e341defbc20f33e4fe189d8ad47edacb1d
manueltirado <manueltirado@JH9KW4RWNH-A098.local>
2024-06-03 20:11:19 +0200

    init

---
+++ CleanArchitecture/CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by manueltirado on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
